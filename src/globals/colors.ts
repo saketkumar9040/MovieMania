9c236c1a34b6942d59422bf0910974cb7cca9eb7
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-12-10 11:32:59 +0530

    feat - added globals folder in project

--- src/globals/colors.ts
+++ src/globals/colors.ts
@@ -1,29 +1,29 @@
 
 interface Color {
-    Black : String;
-    BlackRGB10 : String;
-    Orange : String;
-    OrangeRGBA0 :String;
-    Grey :String;
-    DarkGrey:String;
-    Yellow:String;
-    White:String;
-    WhiteRGBA75:String;
-    WhiteRGBA50:String;
-    WhiteRGBA32:String;
-    WhiteRGBA15:String;
+    Black: String;
+    BlackRGB10: String;
+    Orange: String;
+    OrangeRGBA0: String;
+    Grey: String;
+    DarkGrey: String;
+    Yellow: String;
+    White: String;
+    WhiteRGBA75: String;
+    WhiteRGBA50: String;
+    WhiteRGBA32: String;
+    WhiteRGBA15: String;
 };
-export const COLORS : Color = {
-    Black  :"#000000",
-    BlackRGB10 : "rgba(0,0,0,0.1)",
-    Orange : #ff5524,
-    OrangeRGBA0 :"rgba(255,85,36,0)",
-    Grey :"#333333",
-    DarkGrey:"#0b0b0b",
-    Yellow:"#e1cd17",
-    White:"#ffffff",
-    WhiteRGBA75:"rgba(255,255,255,0.75)",
-    WhiteRGBA50:"rgba(255,255,255,0.50)",
-    WhiteRGBA32:"rgba(255,255,255,0.32)",
-    WhiteRGBA15:"rgba(255,255,255,0.15)",
+export const COLORS: Color = {
+    Black: "#000000",
+    BlackRGB10: "rgba(0,0,0,0.1)",
+    Orange: #ff5524,
+    OrangeRGBA0: "rgba(255,85,36,0)",
+    Grey: "#333333",
+    DarkGrey: "#0b0b0b",
+    Yellow: "#e1cd17",
+    White: "#ffffff",
+    WhiteRGBA75: "rgba(255,255,255,0.75)",
+    WhiteRGBA50: "rgba(255,255,255,0.50)",
+    WhiteRGBA32: "rgba(255,255,255,0.32)",
+    WhiteRGBA15: "rgba(255,255,255,0.15)",
 };
