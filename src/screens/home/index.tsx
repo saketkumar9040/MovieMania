1049f46962ed7dd9f5b07f3d1c7ba25969849cd7
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-12-10 21:23:06 +0530

    feat - added activity indicator when there is no movie lists

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity,Dimensions } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Dimensions, ActivityIndicator } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
@@ -7,17 +7,31 @@ import styles from './style';
 import { COLORS } from '../../globals/colors';
 import { FONTSIZE } from '../../globals/fonts';
 
-import { 
-  upcomingMoviesURL, 
-  nowPlayingMoviesURL, 
-  popularMoviesURL, 
-  baseImagePath } from '../../api/apiCalls';
+import {
+  upcomingMoviesURL,
+  nowPlayingMoviesURL,
+  popularMoviesURL,
+  baseImagePath
+} from '../../api/apiCalls';
 
-  const {width,height} =Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 const HomeScreen = ({ navigationt, route }: any) => {
   const [searchText, setSearchText] = useState("");
-  const []
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] = useState<any>(undefined);
+  const [popularMoviesList, setPopularMoviesList] = useState<any>(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] = useState<any>(undefined);
+
+  if (nowPlayingMoviesList === undefined || nowPlayingMoviesList === null || popularMoviesList === undefined || popularMoviesList === null) {
+    return (
+      <ActivityIndicator
+        size={60}
+        color={COLORS.Orange}
+        style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: COLORS.Black }}
+      />
+    )
+  }
+
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.searchContainer}>
