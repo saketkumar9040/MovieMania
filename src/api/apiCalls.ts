c04d81116bddbb6577f68c2d108be7d821d4f621
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2023-12-10 13:54:38 +0530

    feat - added api folder in project

--- src/api/apiCalls.ts
+++ src/api/apiCalls.ts
@@ -1,2 +1,13 @@
 const apiKey: string = "58a4977899e4dbd38c7683cfb182db92";
-export const nowPlayingMovies =`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
+export const nowPlayingMoviesURL: string = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+export const upcomingMoviesURL: string = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`;
+export const popularMoviesURL: string = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
+export const searchMovies = (keyword: string) => {
+    return `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyword}`;
+};
+export const movieDetails = (id:number) => {
+    return `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`;
+};
+export const movieCastDetails = (id:number) => {
+    return ``
+}
